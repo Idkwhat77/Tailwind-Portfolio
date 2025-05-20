4a20da803f2a91dc4f7adb8584b52ac8b6eed8a3
Idkwhat77 <swissarchipelago@gmail.com>
2025-05-20 22:53:51 +0800

    added sidebar for mobile, changed a lot of stuff to make it prettier

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        bounce: 'bounce 1s infinite',
+      },
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+            color: 'inherit',
+            a: {
+              color: 'inherit',
+              textDecoration: 'underline',
+              fontWeight: '500',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
